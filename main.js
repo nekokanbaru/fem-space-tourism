92dfc7db9145ccac0e17e2dd63885dc383d0b7bc
Ognjen Tabajica <tabajicaognjen@gmail.com>
2022-08-15 13:40:54 +0200

    add technology page

--- main.js
+++ main.js
@@ -157,3 +157,56 @@ if (dot1) {
       "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ";
   });
 }
+
+//technology content change
+
+const techImage = document.querySelector("#techImage");
+const techTitle = document.querySelector("#technology-title");
+const techDescription = document.querySelector("#technology-description");
+const techImgResponsive = document.querySelector("#techImageResponsive");
+
+const techNum1 = document.querySelector("#techNumbers1");
+const techNum2 = document.querySelector("#techNumbers2");
+const techNum3 = document.querySelector("#techNumbers3");
+
+if (techNum1) {
+  techNum1.toggleAttribute("techNum-active");
+
+  techNum1.addEventListener("click", function changeContent() {
+    techNum2.removeAttribute("techNum-active");
+    techNum3.removeAttribute("techNum-active");
+    techNum1.toggleAttribute("techNum-active");
+
+    techImage.src = "assets/technology/image-launch-vehicle-landscape.jpg";
+    techImgResponsive.srcset =
+      "assets/technology/image-launch-vehicle-portrait.jpg";
+    techTitle.innerHTML = "LAUNCH VEHICLE";
+    techDescription.innerHTML =
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!";
+  });
+
+  techNum2.addEventListener("click", function changeContent() {
+    techNum1.removeAttribute("techNum-active");
+    techNum3.removeAttribute("techNum-active");
+    techNum2.toggleAttribute("techNum-active");
+
+    techImage.src = "assets/technology/image-spaceport-landscape.jpg";
+    techImgResponsive.srcset = "assets/technology/image-spaceport-portrait.jpg";
+    techTitle.innerHTML = "SPACEPORT";
+    techDescription.innerHTML =
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.";
+  });
+
+  techNum3.addEventListener("click", function changeContent() {
+    techNum2.removeAttribute("techNum-active");
+    techNum1.removeAttribute("techNum-active");
+    techNum3.toggleAttribute("techNum-active");
+
+    techImage.src = "assets/technology/image-space-capsule-landscape.jpg";
+    techImgResponsive.srcset =
+      "assets/technology/image-space-capsule-portrait.jpg";
+    techTitle.innerHTML = "SPACE CAPSULE";
+    techDescription.innerHTML =
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.";
+  });
+}
